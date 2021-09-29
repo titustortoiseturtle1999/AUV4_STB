ea1e3a2ad764c101f022e8559e35610fbfda1276
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-09-29 09:59:04 +0800

    Initial commit

---
+++ development/STB_4_1.1_debug/auv_4.0_can_def.h
@@ -0,0 +1,32 @@
+#ifndef _DEFINE_H_
+#define _DEFINE_H_
+
+#define CAN_thruster_1 0
+#define CAN_thruster_2 1
+#define CAN_ACT_CONTROL 2
+#define CAN_PCB_STAT 3
+#define CAN_HEARTBEAT 4
+#define CAN_CPU_TEMP 5
+#define CAN_TAB_CONFIG 6
+#define CAN_STB_CONFIG 7
+#define CAN_PMB1_CONFIG 8
+#define CAN_PMB2_CONFIG 9
+#define CAN_ALL_CONFIG 10
+#define CAN_STB_SENS 11
+#define CAN_STB_UP 12
+#define CAN_STB_GYRO 13
+#define CAN_STB_ACC 14
+#define CAN_STB_MAG 15
+#define CAN_STB_STAT 16
+#define CAN_THT_STAT_1 17
+#define CAN_THT_STAT_2 18
+#define CAN_THT_PWM_1 19
+#define CAN_THT_PWM_2 20
+#define CAN_ACT_STAT 21
+#define CAN_TAB_STAT 22
+#define CAN_BATT1_STAT 23
+#define CAN_PMB1_STAT 24
+#define CAN_BATT2_STAT 25
+#define CAN_PMB2_STAT 26
+
+#endif
